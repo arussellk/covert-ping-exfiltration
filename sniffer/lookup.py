8b4d2c8f2670c8a4633b6ff24ed8a49ba9f773e9
Russell Kennington <arussellk@gmail.com>
2020-04-17 11:33:29 -0600

    Add mode->nibble method

---
+++ sniffer/lookup.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+from typing import List,Tuple,Optional
+
+def modes_to_nibbles(
+        modes: List[Optional[Tuple[str,int]]]) -> List[Optional[bytes]]:
+    """
+    Note that all bytes are <=15.
+    The largest nibble is 0b0000_1111 = 0x0f = 15
+    """
+
+    return [mode_to_nibble(m) for m in modes]
+
+def mode_to_nibble(mode: Optional[Tuple[str,int]]) -> Optional[bytes]:
+    lookup = defaultdict(lambda: None, {
+        ('192.168.1.2',  3): b'\x00',
+        ('192.168.1.2',  4): b'\x01',
+        ('192.168.1.2',  5): b'\x02',
+        ('192.168.1.2',  7): b'\x03',
+
+        ('192.168.1.3',  3): b'\x04',
+        ('192.168.1.3',  4): b'\x05',
+        ('192.168.1.3',  5): b'\x06',
+        ('192.168.1.3',  7): b'\x07',
+
+        ('192.168.1.4',  3): b'\x08',
+        ('192.168.1.4',  4): b'\x09',
+        ('192.168.1.4',  5): b'\x0a',
+        ('192.168.1.4',  7): b'\x0b',
+
+        ('192.168.1.5',  3): b'\x0c',
+        ('192.168.1.5',  4): b'\x0d',
+        ('192.168.1.5',  5): b'\x0e',
+        ('192.168.1.5',  7): b'\x0f',
+
+        # ('192.168.1.10', 5): b'\x00',
+    })
+
+    return lookup[mode]
--- sniffer/main.py
+++ sniffer/main.py
@@ -1,12 +1,18 @@
+from typing import List,Tuple,Optional
 from loader import load_icmp_packets
 from extractor import get_modes
+from lookup import modes_to_nibbles
 
 def main():
     pkts = load_icmp_packets('../pcap_samples/ICMP_test1.pcap')
     print(pkts)
 
+    modes: List[Optional[Tuple[str,int]]]
     modes = get_modes(pkts, '192.168.1.2')
     print(modes)
+
+    nibbles = modes_to_nibbles(modes)
+    print(nibbles)
 
 if __name__ == '__main__':
     main()
